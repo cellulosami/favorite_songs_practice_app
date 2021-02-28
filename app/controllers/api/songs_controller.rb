ac922f2a08cde62263ba0bb8d875df42af8f7989
austin shields <ajshields196@gmail.com>
2021-02-28 13:26:48 -0500

    add songs show method

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -3,4 +3,24 @@ class Api::SongsController < ApplicationController
     @songs = Song.all
     render 'index.json.jb'
   end
+
+  def show
+    @song = Song.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+
+
+  # def show
+  #   render 'show.json.jb'
+  # end
+
+
+  # def show
+  #   render 'show.json.jb'
+  # end
+
+
+  # def show
+  #   render 'show.json.jb'
+  # end
 end
