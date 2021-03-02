abc7f08de5a1d61f5a65f3e1229e84f6161e24e5
austin shields <ajshields196@gmail.com>
2021-03-01 19:28:05 -0500

    add songs update method

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -22,9 +22,15 @@ class Api::SongsController < ApplicationController
   end
 
 
-  # def show
-  #   render 'show.json.jb'
-  # end
+  def update
+    @song = Song.find_by(id: params[:id])
+    @song[:title] = params[:title] || @song[:title]
+    @song[:album] = params[:album] || @song[:album]
+    @song[:artist] = params[:artist] || @song[:artist]
+    @song[:year] = params[:year] || @song[:year]
+    @song.save
+    render 'show.json.jb'
+  end
 
 
   # def show
